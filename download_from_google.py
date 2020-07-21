677131d778a299d98348c55952a3ea848176eee7
ankitlohiya212 <58203694+ankitlohiya212@users.noreply.github.com>
2020-07-21 16:55:47 +0530

    Update download_from_google.py

--- download_from_google.py
+++ download_from_google.py
@@ -4,7 +4,7 @@ from time import sleep
 search = "your_query"
 num_imgs = 100
 
-driver = webdriver.Chrome('path\to\chromedriver_win32/chromedriver')
+driver = webdriver.Chrome('path/to/chromedriver_win32/chromedriver')
 url = "https://www.google.com/search?q={}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwikzYbF1t3qAhVRX30KHRs7CWMQ_AUoAXoECCAQAw&biw=1366&bih=657".format(search)
 driver.get(url)
 
@@ -12,7 +12,7 @@ for i in range(1, num_imgs+1):
     try:
         xpath = "/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[{}]/a[1]/div[1]/img".format(i)
         element = driver.find_element_by_xpath(xpath)
-        element.screenshot("downloaded_from_google/"+search+str(i)+".png")
+		element.screenshot("path/to/directory/"+search+str(i)+".png")
         sleep(0.1)
         print(i)
     except:
