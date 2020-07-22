7cbae666be19cfbcc6faba250836baa62371981f
ankitlohiya212 <58203694+ankitlohiya212@users.noreply.github.com>
2020-07-22 10:35:13 +0530

    Update download_from_google.py

--- download_from_google.py
+++ download_from_google.py
@@ -12,7 +12,7 @@ for i in range(1, num_imgs+1):
     try:
         xpath = "/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[{}]/a[1]/div[1]/img".format(i)
         element = driver.find_element_by_xpath(xpath)
-		element.screenshot("path/to/directory/"+search+str(i)+".png")
+	element.screenshot("path/to/directory/"+search+str(i)+".png")
         sleep(0.1)
         print(i)
     except:
